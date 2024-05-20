72819f72add6110cfcd37bca6b0f97a0e34706fe
Víctor Olvera <vhocar@gmail.com>
2024-05-20 11:38:59 -0600

    Added folder structure and dummy files for git pushin

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'], exclude: ['src'] })],
   build: {
    lib: {
       entry: resolve( __dirname, 'lib/main.ts' ),
       formats: ['es']
    },
    copyPublicDir: false,
+   rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+   }
   }
 })
