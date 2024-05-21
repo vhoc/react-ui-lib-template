e3e267a9bba9538043a4f0e49701f675599053dd
Víctor Olvera <vhocar@gmail.com>
2024-05-21 12:24:37 -0600

    Upgrades: - Added recommended global styles - Added Intro page.

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,4 @@
+import '../lib/global.css'
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
--- lib/components/ui/buttons/Button/Button.stories.ts
+++ lib/components/ui/buttons/Button/Button.stories.ts
@@ -14,14 +14,12 @@ const meta = {
    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
    tags: ['autodocs'],
    // More on argTypes: https://storybook.js.org/docs/api/argtypes
-   // argTypes: {
-   //    //  backgroundColor: { control: 'color' },
-   //    // disabled: { control: { type: 'boolean' } },
-   // },
+   argTypes: {
+      disabled: { control: { type: 'boolean' } },
+   },
    // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
    args: {
       onClick: fn(),
-      disabled: false,
    },
 } satisfies Meta<typeof Button>;
 
@@ -31,7 +29,7 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
    args: {
-      //  primary: true,
+      disabled: false,
       children: 'Button',
    },
 };
