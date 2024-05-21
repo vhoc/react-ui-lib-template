c0112b8092521bc628c1d6f3158ec00dcf9999bc
Víctor Olvera <vhocar@gmail.com>
2024-05-21 11:34:36 -0600

    Before upgrade

--- lib/components/ui/buttons/Button/Button.stories.ts
+++ lib/components/ui/buttons/Button/Button.stories.ts
@@ -1,33 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 
-import { Button, ButtonProps } from './Button';
+import { Button, } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-   //  backgroundColor: { control: 'color' },
-  },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
-  args: { onClick: fn() },
-} satisfies Meta<ButtonProps>;
+   title: 'Example/Button',
+   component: Button,
+   parameters: {
+      // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+      layout: 'centered',
+   },
+   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+   tags: ['autodocs'],
+   // More on argTypes: https://storybook.js.org/docs/api/argtypes
+   // argTypes: {
+   //    //  backgroundColor: { control: 'color' },
+   //    // disabled: { control: { type: 'boolean' } },
+   // },
+   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+   args: {
+      onClick: fn(),
+      disabled: false,
+   },
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Default: Story = {
-  args: {
-   //  primary: true,
-    children: 'Button',
-  },
+   args: {
+      //  primary: true,
+      children: 'Button',
+   },
+};
+
+export const Disabled: Story = {
+   args: {
+      disabled: true,
+      children: 'Button',
+   },
 };
--- lib/components/ui/buttons/Button/Button.tsx
+++ lib/components/ui/buttons/Button/Button.tsx
@@ -1,21 +1,38 @@
 import styles from './Button.module.css'
 
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+   /**
+    * Toggles the "disabled" state of the button.
+    */
+   disabled?: boolean | undefined
+
    /** 
     * Any kind of component inside if the button
     */
    children?: React.ReactNode | Array<React.ReactNode> | undefined
 }
 
-export const Button: React.FC<ButtonProps> = ( props ): JSX.Element => {
-   const { className, ...restProps } = props
+export const Button: React.FC<ButtonProps> = ({
+   className,
+   disabled = false,
+   children,
+   ...props
+}): JSX.Element => {
+
+   // Default props
+   // const { className,
+   //    disabled = false,
+   //    children,
+   //    ...rest
+   // } = props
 
    return (
       <button
-         className={ `${ className } ${ styles.button }` }
-         { ...restProps }
+         disabled={disabled}
+         className={`${className} ${styles.button}`}
+         {...props}
       >
-         { props.children }
+         {children}
       </button>
    )
 
